7caa864769016a9a9052973df809575b2a67c166
yurannhassengo <yurannhassengo@gmail.com>
2024-08-20 20:10:36 -0700

    add slider

--- src/components/workExperience/workExperience.jsx
+++ src/components/workExperience/workExperience.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './workExperience.css'
 import { WORK_EXPERIENCE } from '../../utils/data'
 import { ExperienceCard } from './experienceCard/experienceCard'
+import Slider from 'react-slick'
 
 export const WorkExperience = () => {
+
+    const sliderRef = useRef();
+
+    const settings ={
+        dots:false,
+        infinite:true,
+        speed:500,
+        slidesToShow:2,
+        slidesToScroll:1,
+        arrows:false,
+        responsive: [
+            {
+                breakpoint:769,
+                settings:{
+                    slidesToShow:1,
+                    slidesToScroll:1,
+
+                },
+            },
+        ],
+    };
+
   return (
     <section className="experience-container">
         <h5>Work Experience</h5>
 
         <div className="experience-content">
-
+            <Slider ref={sliderRef} {...settings}>
             {WORK_EXPERIENCE.map((item) => (
                 <ExperienceCard key={item.title} details={item}/>
             ))}
+            </Slider>
         </div>
     </section>
   )
